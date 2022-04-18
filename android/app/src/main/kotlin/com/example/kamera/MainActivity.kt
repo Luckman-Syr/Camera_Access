5ba57ca7c4dcb9bdc7b769c383ca85090399803f
Luckman-Syr <luckmannathan46@gmail.com>
2022-04-18 10:25:02 +0700

    'Kumpul'

---
+++ android/app/src/main/kotlin/com/example/kamera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kamera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  File? imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Njajal Kamera Maseh'),
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (imageFile != null)
+              Container(
+                width: 640,
+                height: 480,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  image: DecorationImage(
+                      image: FileImage(imageFile!), fit: BoxFit.cover),
+                  border: Border.all(width: 8, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(270),
+                ),
+              )
+            else
+              Container(
+                width: 640,
+                height: 480,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(width: 8, color: Colors.black12),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: const Text(
+                  'Image should appear here',
+                  style: TextStyle(fontSize: 26),
+                ),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      style: ButtonStyle(),
+                      onPressed: () => getImage(source: ImageSource.camera),
+                      child: const Text('Ambil Gambar',
+                          style: TextStyle(fontSize: 18, color: Colors.white))),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () => getImage(source: ImageSource.gallery),
+                      child: const Text('Select Image',
+                          style: TextStyle(fontSize: 18))),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getImage({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(
+        source: source,
+        maxWidth: 640,
+        maxHeight: 480,
+        imageQuality: 70 //0 - 100
+        );
+
+    if (file?.path != null) {
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
+  }
+}
